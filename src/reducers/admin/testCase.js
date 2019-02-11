e787cf133be80d5f37536cb71238a589ecc40c9d
kilimangara <kilimangara@yandex.ru>
2019-02-11 14:46:17 +0300

    wip testcases

--- src/reducers/admin/testCase.js
+++ src/reducers/admin/testCase.js
@@ -3,5 +3,31 @@ import {
   deleteTestCase,
   updateTestCase,
   deleteAnswer,
-  deleteTest
+  deleteTest,
+  getTestCases
 } from '../../api'
+
+
+export const loadTestCases = (themeId) => {
+  return getTestCases(themeId)
+}
+
+export const newTestBlock = (themeId, data) => {
+  return createTestCase(themeId, data)
+}
+
+export const removeTestCase = (themeId, testCaseId) => {
+  return deleteTestCase(themeId, testCaseId)
+}
+
+export const putTestCase = (themeId, testCaseId, data) => {
+  return updateTestCase(themeId, testCaseId, data)
+}
+
+export const removeAnswer = (testId, answerId) => {
+  return deleteAnswer(testId, answerId)
+}
+
+export const removeTest = (testCaseId, testId) => {
+  return deleteTest(testCaseId, testId)
+}
