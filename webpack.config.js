dedf85564b21fbf589e1aced48a95c4d3f760752
Artem <attemka@gmail.com>
2019-02-19 18:56:14 +0300

    minor bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,65 @@
-"use strict";
-var webpack = require("webpack");
-var path = require("path");
-var loaders = require("./webpack.loaders");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+'use strict'
+var webpack = require('webpack')
+var path = require('path')
+var loaders = require('./webpack.loaders')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const HOST = process.env.HOST || "127.0.0.1";
-const PORT = process.env.PORT || "3000";
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || '3000'
 
-module.exports = env =>
-{
-  return {
-    entry: [
-      "react-hot-loader/patch",
-      "./src/index.js",
-    ],
-    devtool: process.env.WEBPACK_DEVTOOL || "eval-source-map",
-    output: {
-      publicPath :'/',
-      path: path.resolve('./dist'),
-      filename: "app_bundle_dev.js"
-    },
-    resolve: {
-      extensions: [".js", ".jsx", ".coffee"]
-    },
-    module: {
-      rules: loaders
-    },
-    devServer: {
-      contentBase: './public',
-      index: '/index_student.html',
-      // enable HMR
-      hot: true,
-      // embed the webpack-dev-server runtime into the bundle
-      inline: true,
-      // serve index.html in place of 404 responses to allow HTML5 history
-      historyApiFallback: {
-        index: '/index_student.html',
-      },
-      compress: true,
-      port: PORT,
-      host: HOST
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        __DEV__: env.DEV,
-        __CURRENT_APP__: JSON.stringify('USER_APP')
-      }),
-      new webpack.NoEmitOnErrorsPlugin(),
-      new webpack.NamedModulesPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new ExtractTextPlugin({
-        filename: "styles.css",
-      }),
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-        title: 'Evys',
-        filename: 'index_student.html',
-        description: 'Обучающая платформа Evys.',
-        files: {
-          css: ["styles.css"],
-          js: [ "app_bundle_dev.js"],
-        }
-      }),
-    ]
-  };
-};
+module.exports = env => {
+    return {
+        entry: ['react-hot-loader/patch', './src/index.js'],
+        mode: 'development',
+        devtool: process.env.WEBPACK_DEVTOOL || 'eval-source-map',
+        output: {
+            publicPath: '/',
+            path: path.resolve('./dist'),
+            filename: 'app_bundle_dev.js'
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.coffee']
+        },
+        module: {
+            rules: loaders
+        },
+        devServer: {
+            contentBase: './public',
+            index: '/index_student.html',
+            // enable HMR
+            hot: true,
+            // embed the webpack-dev-server runtime into the bundle
+            inline: true,
+            // serve index.html in place of 404 responses to allow HTML5 history
+            historyApiFallback: {
+                index: '/index_student.html'
+            },
+            compress: true,
+            port: PORT,
+            host: HOST
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                __DEV__: env.DEV,
+                __CURRENT_APP__: JSON.stringify('USER_APP')
+            }),
+            new webpack.NoEmitOnErrorsPlugin(),
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin(),
+            new ExtractTextPlugin({
+                filename: 'styles.css'
+            }),
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                title: 'Evys',
+                filename: 'index_student.html',
+                description: 'Обучающая платформа Evys.',
+                files: {
+                    css: ['styles.css'],
+                    js: ['app_bundle_dev.js']
+                }
+            })
+        ]
+    }
+}
