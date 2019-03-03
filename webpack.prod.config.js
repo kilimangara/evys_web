a1f23210beb73b6b8ce37896475701e1ec0eae2a
kilimangara <kilimangara@yandex.ru>
2019-03-03 15:52:14 +0300

    подчищаем продакшен конфиги

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,55 +1,63 @@
-"use strict";
-var webpack = require("webpack");
-var path = require("path");
-var loaders = require("./webpack.loaders");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+'use strict'
+var webpack = require('webpack')
+var path = require('path')
+var loaders = require('./webpack.loaders')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 var CompressionPlugin = require('compression-webpack-plugin')
+var HtmlWebpackChangeAssetsExtensionPlugin = require('html-webpack-change-assets-extension-plugin')
 
-module.exports = env =>
-{
-  return {
-    entry: [
-      "./src/index.js",
-    ],
-    output: {
-      publicPath : '/dist/',
-      path: path.resolve('./dist/'),
-      filename: "[hash].app_bundle.js"
-    },
-    resolve: {
-      extensions: [".js", ".jsx", ".coffee"]
-    },
-    module: {
-      rules: loaders
-    },
-    plugins: [
-      new ExtractTextPlugin({
-        filename: "[hash].styles.css"
-      }),
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        __DEV__: false,
-        __CURRENT_APP__: JSON.stringify('USER_APP')
-      }),
-      new webpack.optimize.AggressiveMergingPlugin(),
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-        title: 'Evys',
-        filename: 'index_student.html',
-        description: 'Обучающая платформа Evys.',
-        files: {
-          css: ["[hash].styles.css"],
-          js: [ "[hash].app_bundle_dev.js"],
-        }
-      }),
-      // new CompressionPlugin({
-      //   asset: "[path].gz[query]",
-      //   algorithm: "gzip",
-      //   test: /\.js$|\.css$|\.html$/,
-      //   threshold: 10240,
-      //   minRatio: 0.8
-      // })
-    ]
-  };
-};
+module.exports = env => {
+    return {
+        entry: ['./src/index.js'],
+        output: {
+            publicPath: '/dist/',
+            path: path.resolve('./dist/'),
+            filename: '[hash].app_bundle.js'
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.coffee']
+        },
+        module: {
+            rules: loaders
+        },
+        optimization: {
+            splitChunks: {
+                chunks: 'async',
+                minSize: 30000,
+                maxSize: 0,
+                minChunks: 1,
+                maxAsyncRequests: 5,
+                maxInitialRequests: 3,
+                automaticNameDelimiter: '~',
+                name: true
+            }
+        },
+        plugins: [
+            new ExtractTextPlugin({
+                filename: '[hash].styles.css'
+            }),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production'),
+                __DEV__: false,
+                __CURRENT_APP__: JSON.stringify('USER_APP')
+            }),
+            new webpack.optimize.AggressiveMergingPlugin(),
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                title: 'Evys',
+                filename: 'index_student.html',
+                description: 'Обучающая платформа Evys.',
+                jsExtension: '.gz'
+            }),
+            new CompressionPlugin({
+                asset: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: /\.js$/,
+                threshold: 10240,
+                minRatio: 0.8
+            }),
+            new HtmlWebpackChangeAssetsExtensionPlugin()
+        ]
+    }
+}
