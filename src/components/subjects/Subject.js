6d8afc6431267aea30b18790c3d7438870350176
Artem <attemka@gmail.com>
2019-04-02 19:15:39 +0300

    fixed course image in admin

--- src/components/subjects/Subject.js
+++ src/components/subjects/Subject.js
@@ -8,29 +8,24 @@ import styled from 'styled-components'
 const DEFAULT_IMG_SRC = 'http://golos-vremeni.ru/media/cache/ad/f6/93/0b/e0/55/adf6930be0555d915298b4f92934095f.jpg'
 
 const Wrapper = styled(HoverPaper)`
-  height: 300px;
-  width: 350px;
-  background: ${({image}) => `url(${image}) no-repeat center center`};
-  background-size: cover;
+    height: 300px;
+    width: 350px;
+    background: ${({ image }) => `url(${image}) no-repeat center center`};
+    background-size: cover;
 `
 
 Wrapper.defaultProps = {
-  image: DEFAULT_IMG_SRC
+    image: DEFAULT_IMG_SRC
 }
 
 export const Subject = ({ subject, index, onClickSubjectInfo, onClickSubject }) => (
-    <GridListTile key={subject.id} cols={1} component='div'>
-        <Wrapper onClick={() => onClickSubjectInfo(subject)} image={subject.category.image}>
+    <GridListTile key={subject.id} cols={1} component="div">
+        <Wrapper onClick={() => onClickSubjectInfo(subject)} image={subject.mainImage || subject.category.image}>
             <GridListTileBar
                 title={subject.subject}
-                subtitle={<p></p>}
-                actionIcon={
-                    <IconButton
-                        onClick={() => onClickSubject(subject.id)}
-                    />
-                }
-            >
-            </GridListTileBar>
+                subtitle={<p />}
+                actionIcon={<IconButton onClick={() => onClickSubject(subject.id)} />}
+            />
         </Wrapper>
     </GridListTile>
 )
